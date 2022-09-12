ff7c9df131af6aa58e6008ea60ab7993c51a5df8
RadhaPandurangGarad <radhagarad21@gmail.com>
2022-09-12 10:00:28 +0530

    End of Section 3

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,9 +7,15 @@ namespace API.Entities
 {
     public class AppUser
     {
-       public int Id { get; set; }
+        internal object username;
+
+        public int Id { get; set; }
 
        public string UserName { get; set;}
+
+       public byte[] PasswordHash { get; set;}
+
+       public byte[] PasswordSalt { get; set;}
        
     }
 }
